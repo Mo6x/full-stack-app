644c0ee85b2ea4c7a0a87476af201b929c8c3b05
mac@Dev <christmoses1999@gmail.com>
2023-04-24 01:06:30 +0100

    Import all my routes in App.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,6 +2,12 @@ import express from 'express';
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
+import gigRoute from "./routes/gigRoute.js";
+import reviewRoute from "./routes/reviewRoute.js";
+import orderRoute from "./routes/orderRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import conversationRoute from "./routes/coversationRoute.js";
+
 
 
 const app = express();
@@ -10,7 +16,6 @@ mongoose.set("strictQuery", true);
 
 
 const connect = async ()=> {
-
 try {
   await mongoose.connect(process.env.MONGO);
   console.log("Connected to mongoDB");
@@ -19,9 +24,13 @@ try {
 }}
 
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/reviews", reviewRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute);
+
 
 app.listen(5000, () => {
   connect();
