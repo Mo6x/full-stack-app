fbda7a6d0ee674bcece51579d958bed2355841c5
mac@Dev <christmoses1999@gmail.com>
2023-05-11 00:21:02 +0100

    Design logic for createReviw in controller files

--- Server/src/controllers/reviewController.js
+++ Server/src/controllers/reviewController.js
@@ -1,3 +1,54 @@
-export const fn = async (req, res) => {
+import createError from "../utils/createError";
+import Review from "../models/reviewModel.js";
+import Gig from "../models/gigModel.js";
+
+
+
+export const createReview = async (req, res, next) => {
+    if (req.isSeller)
+    return next(createError(403, "Sellers can't create a review!"));
+
+    const newReview = new Review({
+       userId: req.userId,
+       gigId: req.body.userId,
+       desc: req.body.desc,
+       star: req.body.star,
+    });
+
+    try{
+    const review = await Review.findOne({
+        gigId: req.body.gigId,
+        userId: req.body.userId,
+    });
+    if (review)
+    return next(createError(403, "You have already created a review for this gig"));
+
+    const saveReview = await newReview.save();
+    await Gig.findByIdAndUpdate(req.body.gigId, {
+        $inc: { totalStars: req.body.star, starNumber: 1 }, 
+    });
+    res.status(201).send(saveReview);
+    }catch(err) {
+      next(err);
+    }
+};
+
+
+export const getReviews = async (req, res, next) => {
     
-}
+    try{
+
+    }catch(err) {
+      next(err);
+    }
+};
+
+
+export const deleteReviews = async (req, res, next) => {
+    
+    try{
+
+    }catch(err) {
+      next(err);
+    }
+};
