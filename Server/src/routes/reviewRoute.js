99fd3483fe4edf4e5b7e3cf0d65fa2519c2e4077
mac@Dev <christmoses1999@gmail.com>
2023-05-10 23:07:14 +0100

    design all the review Router and import from controllers

--- Server/src/routes/reviewRoute.js
+++ Server/src/routes/reviewRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
+import { verifyToken } from "../middleware/jwt";
+import {
+    createReview,
+    getReviews,
+    deleteReviews,
+} from "../controllers/reviewController.js";
+
 
 
 const router = express.Router();
 
+router.post("/", verifyToken, createReview);
+router.get("/:id", getReviews);
+router.delete("/:id", deleteReviews);
 
 
 export default router;
