49c2dd8d4faadd750d586789d3a0762e0f047f7b
mac@Dev <christmoses1999@gmail.com>
2023-05-16 11:54:51 +0100

    design my rewieww component

--- Client/src/components/reviews/Reviews.jsx
+++ Client/src/components/reviews/Reviews.jsx
@@ -14,12 +14,35 @@ const Reviews = ({gigId}) => {
             return res.data;
         }),
     });
-   return (
 
+    
+    const handleSubmit = (e) => {
+        e.preventDefualt();
+        const desc = e.target[0].value;
+        const star = e.target[1].value;
+        mutation.nutata({ desc, star});
+    } 
+
+
+   return (
     <div className="reviews">
             <h2>Reviews</h2>
             {isLoading ? "loading" : error ? "Something went wrong!" : 
             data.map((review) => <Review key={review._id} review = {review} /> )}
+            <div className="add">
+               <h3>Add a review</h3>
+               <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Share your opinion" />
+                <select name="" id="">
+                    <opinion value={1}>1</opinion>
+                    <opinion value={2}>2</opinion>
+                    <opinion value={3}>3</opinion>
+                    <opinion value={4}>4</opinion>
+                    <opinion value={5}>5</opinion>
+                </select>
+                <botton>Send</botton>
+               </form>
+            </div>
           </div>
   ) 
 }
