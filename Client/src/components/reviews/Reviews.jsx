f7d1c5c268990741835921352e8c3f89bf2cfd29
mac@Dev <christmoses1999@gmail.com>
2023-05-29 22:18:15 +0100

    worked on the Reviews,jsx

--- Client/src/components/reviews/Reviews.jsx
+++ Client/src/components/reviews/Reviews.jsx
@@ -43,7 +43,7 @@ const Reviews = ({gigId}) => {
             data.map((review) => <Review key={review._id} review = {review} /> )}
             <div className="add">
                <h3>Add a review</h3>
-               <form action="" onSubmit={handleSubmit}>
+               <form action="" className="addForm"  onSubmit={handleSubmit}>
                 <input type="text" placeholder="Share your opinion" />
                 <select name="" id="">
                     <opinion value={1}>1</opinion>
